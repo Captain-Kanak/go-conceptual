766489bd2cdd5bdd8599ef87735066efa2c4eae3
Captain-Kanak <kanakroy835@gmail.com>
2026-06-20 13:09:19 +0600

    successfully complete repository, service and handler

--- internal/domain/user/handler.go
+++ internal/domain/user/handler.go
@@ -1,1 +1,15 @@
 package user
+
+type handler struct {
+	service service
+}
+
+func NewHandler(service service) *handler {
+	return &handler{service}
+}
+
+func (h *handler) RegisterUser() {}
+
+func (h *handler) GetAllUsers() []User {
+	return []User{}
+}
--- internal/domain/user/service.go
+++ internal/domain/user/service.go
@@ -1,1 +1,22 @@
 package user
+
+type service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) *service {
+	return &service{repo}
+}
+
+func (s *service) RegisterUser() {
+	var user User
+
+	s.repo.Create(user)
+
+}
+
+func (s *service) GetAllUsers() ([]User, error) {
+	users, err := s.repo.GetAll()
+
+	return users, err
+}
